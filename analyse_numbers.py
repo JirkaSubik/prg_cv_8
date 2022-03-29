8b3a6af7d4e0d918e059b7d1464cfd18c9c972fb
Šubík Jiří (229194) <xsubik00@vutbr.cz>
2022-03-29 09:30:05 +0200

    pridal jsem mezery

--- analyse_numbers.py
+++ analyse_numbers.py
@@ -5,5 +5,15 @@ def is_whole_number(number):
 
     return False
 
+
 whole_number = is_whole_number(8)
 print(whole_number)
+
+def is_even_number(number):
+    if number % 2 == 0:
+        return True
+    return False
+
+
+even_number = is_even_number(whole_number)
+print(even_number)
